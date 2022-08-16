6d632a3d89012a9a64a167d9549ce859500d7218
Abdullah-14 <70694432+Abdullah-14@users.noreply.github.com>
2022-08-16 14:26:45 +0300

    Add files via upload

---
+++ cs438/project.js
@@ -0,0 +1,11 @@
+
+function img(anything){
+    document.querySelector('.slide').src = anything;
+}
+function change(change){
+    const line = document.querySelector('.image');
+    line.style.background = change;
+}
+function sign(){
+    bot.addEventListener()
+}
